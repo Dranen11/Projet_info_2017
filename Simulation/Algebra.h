8eafa9818e2f2ac9c10130ff279bae8d116aa195
Mathieu De Bony De Lavergne <mdebony@a-007122.ups.u-psud.fr>
2016-11-30 14:56:59 +0100

    Ajout d'un fichier pour contenir les différentes fonction potentiel

--- Simulation/Algebra.h
+++ Simulation/Algebra.h
@@ -2,5 +2,5 @@
 #include <complex>
 
 
-template<typename Type> std::complex<Type>* solve_cholesky_complex_tri_diag(std::complex<Type>* low_diag, std::complex<Type>* diag, std::complex<Type>* u, std::size_t n)
+template<typename Type> std::complex<Type>* solve_cholesky_complex_tri_diag(std::complex<Type>* low_diag, std::complex<Type>* diag, std::complex<Type>* u, std::size_t n);
 
---
+++ Simulation/Potential.h
@@ -0,0 +1,6 @@
+//Some possible potential
+
+double P_zero(double x, double t, double* param)
+{
+   return 0.;
+}
