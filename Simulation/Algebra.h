ccb9af36c0e24a32985c2b22f7da5d1c34d8379d
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2017-02-18 20:00:38 +0100

    Ajout de la decomposition LU et correction d'erreur et optimisation

--- Simulation/Algebra.h
+++ Simulation/Algebra.h
@@ -1,11 +1,78 @@
 #ifndef ALGEBRA_H
 #define ALGEBRA_H
 
-//Function for solving Cholesky problem
 #include <cstdlib>
-#include <cmath>
 
-//Solving a symmetric linear system using cholesky decomposition
+//Solving a symmetric linear system using alternative cholesky decomposition (LDLt)
+template<typename Type>
+Type* solve_cholesky_tri_diag(Type* low_diag, Type* diag, Type* u, std::size_t n)
+{
+   Type *result, *dec_diag, *dec_low_diag; //Variables dec are think to store the decomposition
+   
+   result = (Type*) malloc(n*sizeof(Type));
+   dec_diag = (Type*) malloc(n*sizeof(Type));
+   dec_low_diag = (Type*) malloc((n-1)*sizeof(Type));
+
+
+   //Decomposition
+   dec_diag[0] = diag[0];
+   for(size_t i = 0; i < (n-1); i++)
+   {
+      dec_low_diag[i] = low_diag[i]/dec_diag[i];
+      dec_diag[i+1] = diag[i+1]-dec_diag[i]*dec_low_diag[i]*dec_low_diag[i];
+   }
+
+   //Solve Equation
+   result[0] = u[0];
+   for(size_t i = 1; i < n; i++)
+   {
+      result[i] = u[i]-dec_low_diag[i-1]*result[i-1];
+   }
+
+   result[n-1] = result[n-1]/dec_diag[n-1];
+   for(size_t i = n-1; i-- > 0;)
+   {
+      result[i] = (result[i]/dec_diag[i])-dec_low_diag[i]*result[i+1];
+   }
+
+   free(dec_diag);
+   free(dec_low_diag);
+   return result;
+}
+
+//Solving a linear system using LU decomposition
+template<typename Type>
+Type* solve_LU_tri_diag(Type* low_diag, Type* diag, Type* upper_diag, Type* u, std::size_t n)
+{
+   Type *result, *result_int, *dec_diag, pivot; //Variables dec are think to store the decomposition
+   
+   result = (Type*) malloc(n*sizeof(Type));
+   result_int = (Type*) malloc(n*sizeof(Type));
+   dec_diag = (Type*) malloc(n*sizeof(Type));
+
+   //Decomposition and first step of solving
+   dec_diag[0] = diag[0];
+   result_int[0] = u[0];
+   for(size_t i = 0; i < (n-1); i++)
+   {
+      pivot = low_diag[i]/dec_diag[i];
+      dec_diag[i+1] = diag[i+1] - pivot*upper_diag[i];
+      result_int[i+1] = u[i+1]-pivot*result_int[i];
+   }
+
+   //Second step of solving
+   result[n-1] = result_int[n-1]/dec_diag[n-1];
+   for(size_t i = n-1; i-- > 0;)
+   {
+      result[i] = (result_int[i]-upper_diag[i]*result[i+1])/dec_diag[i];
+   }
+
+   free(dec_diag);
+   free(result_int);
+   return result;
+}
+
+/*Solving a symmetric linear system using cholesky decomposition (LLt)
 template<typename Type>
 Type* solve_cholesky_tri_diag(Type* low_diag, Type* diag, Type* u, std::size_t n)
 {
@@ -40,43 +107,6 @@ Type* solve_cholesky_tri_diag(Type* low_diag, Type* diag, Type* u, std::size_t n
    free(dec_diag);
    free(dec_low_diag);
    return result;
-}
-
-//Solving a symmetric linear system using cholesky decomposition in the case of the subdiagonal have always the same value
-template<typename Type>
-Type* solve_cholesky_tri_diag(Type low_diag, Type* diag, Type* u, std::size_t n)
-{
-   Type *result, *dec_diag, *dec_low_diag; //Variables dec are think to store the decomposition
-   
-   result = (Type*) malloc(n*sizeof(Type));
-   dec_diag = (Type*) malloc(n*sizeof(Type));
-   dec_low_diag = (Type*) malloc((n-1)*sizeof(Type));
-
-
-   //Decomposition
-   dec_diag[0] = sqrt(low_diag[0]);
-   for(size_t i = 0; i < (n-1); i++)
-   {
-      dec_low_diag[i] = low_diag/dec_diag[i];
-      dec_diag[i+1] = sqrt(diag[i+1]-dec_low_diag[i]*dec_low_diag[i]);
-   }
-
-   //Solve Equation
-   result[0] = u[0];
-   for(size_t i = 1; i < n; i++)
-   {
-      result[i] = (u[i]-dec_low_diag[i-1]*result[i-1])/dec_diag[i];
-   }
-
-   result[n-1] = result[n-1];
-   for(size_t i = n-2; i-- > 0;)
-   {
-      result[i] = (result[i]-dec_low_diag[i+1]*result[i+1])/dec_diag[i];
-   }
-
-   free(dec_diag);
-   free(dec_low_diag);
-   return result;
-}
+}*/
 
 #endif
--- Test/Test_Algebra.cpp
+++ Test/Test_Algebra.cpp
@@ -1,40 +1,122 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include <cmath>
+#include <complex>
+#include <chrono>
+#include <random>
 #include "../Simulation/Algebra.h"
-#define SIZE 10
+
+#define MIN_SIZE 10
+#define MAX_SIZE 300
+#define NUMBER_TEST 100
+#define PRECISION 1e-4
 
 using namespace std;
 
+random_device rd;
+mt19937 gen(rd());
+uniform_real_distribution<> dis(1, 10000);
+double alea()
+{
+   return dis(gen);
+}
+
+void impr_vec(double* vec, size_t n)
+{
+   for(size_t i = 0; i < n; i++)
+   {
+      cout << vec[i] << " ";
+   }
+   cout << endl;
+}
+
+void impr_tri_diag(double* low, double* diag, double* upper, size_t n)
+{
+   for(size_t i = 0; i < n; i++)
+   {
+      for(size_t j = 0; j < n; j++)
+      {
+         if(i == j)
+         {
+            cout << diag[i];
+         }
+         else if(i == j+1)
+         {
+            cout << low[j];
+         }
+         else if (i+1 == j)
+         {
+            cout << upper[i];
+         }
+         else
+         {
+            cout << 0;
+         }
+         cout << " ";
+      }
+      cout << endl;
+   }
+}
+
+
+void test1_unity();
+void test2_random();
+void test3_complex_random();
+
 int main()
 {
-   bool verif = true;
-   double epsilon = 1e-15;
-   double *diag = (double*) malloc(SIZE*sizeof(double)), *sub_diag = (double*) malloc((SIZE-1)*sizeof(double)), *x = (double*) malloc(SIZE*sizeof(double)), *y;
+   cout << "Test decomposition de Cholesky and LU and resolution systeme" << endl;
+   test1_unity();
+   test2_random();
+   test3_complex_random();
 
-   cout << "Test Cholesky decomposition and solving" << endl;
-   cout << "Test 1 : Unite" << endl;
+   return 0;
+}
 
-   for(int i = 0; i < SIZE; i++)
+void test1_unity()
+{
+   bool verif_Cho = true;
+   bool verif_LU = true;
+
+   for(size_t j = MIN_SIZE; j <= MAX_SIZE; j++)
    {
-      x[i] = 1;
-      diag[i] = 1;
-      if(i < (SIZE-1))
+      double epsilon = PRECISION;
+      double *diag = (double*) malloc(j*sizeof(double)), *sub_diag = (double*) malloc((j-1)*sizeof(double)), *upper_diag = (double*) malloc((j-1)*sizeof(double)), *x = (double*) malloc(j*sizeof(double)), *y_LU, *y_Cho;
+
+      for(size_t i = 0; i < j; i++)
       {
-	 sub_diag[i] = 0;
+         x[i] = 1;
+         diag[i] = 1;
+         if(i < (j-1))
+         {
+           sub_diag[i] = 0;
+           upper_diag[i] = 0;
+         }
       }
-   }
-   y* = solve_cholesky_tri_diag<double>(sub_diag, diag, x, std::size_t SIZE);
-   for(int i = 0; i < SIZE; i++)
-   {
-      if(abs(y[i]-1)>epsilon)
+      y_LU = solve_LU_tri_diag<double>(sub_diag, diag, upper_diag, x, j);
+      y_Cho = solve_cholesky_tri_diag<double>(sub_diag, diag, x, j);
+      for(size_t i = 0; i < j; i++)
       {
-	 verif = false;
+         if(abs(y_Cho[i]-1)>epsilon)
+         {
+           verif_Cho = false;
+         }
+         if(abs(y_LU[i]-1)>epsilon)
+         {
+           verif_LU = false;
+         }
       }
+      free(y_Cho);
+      free(y_LU);
+      free(x);
+      free(diag);
+      free(sub_diag);
+      free(upper_diag);
    }
-   free(y);
-   
-   if(verif)
+
+   cout << "Test 1 LU : Unite" << endl;
+   if(verif_LU)
    {
       cout <<"Valide"<<endl;
    }
@@ -43,9 +125,197 @@ int main()
       cout << "Echec"<<endl;
    }
 
-   free(x);
-   free(diag);
-   free(sub_diag);
+   cout << "Test 1 Cholesky : Unite" << endl;
+   if(verif_Cho)
+   {
+      cout <<"Valide"<<endl;
+   }
+   else
+   {
+      cout << "Echec"<<endl;
+   }
+}
 
-   return 0;
+void test2_random()
+{
+   bool verif_Cho = true;
+   bool verif_LU = true;
+   double erreur_cho = 0.;
+   double erreur_LU = 0.;
+   
+   for(size_t j = MIN_SIZE; j <= MAX_SIZE; j++)
+   {
+      for(size_t k = 0; k < NUMBER_TEST; k++)
+      {
+         double epsilon = PRECISION;
+         double *diag = (double*) malloc(j*sizeof(double)), *sub_diag = (double*) malloc((j-1)*sizeof(double)), *upper_diag = (double*) malloc((j-1)*sizeof(double)), *x = (double*) malloc(j*sizeof(double)), *y_LU, *y_Cho;
+   
+         for(size_t i = 0; i < j; i++)
+         {
+            x[i] = alea();
+            diag[i] = alea();
+            if(i < (j-1))
+            {
+              sub_diag[i] = alea();
+              upper_diag[i] = alea();
+            }
+         }
+
+         y_LU = solve_LU_tri_diag<double>(sub_diag, diag, upper_diag, x, j);
+         y_Cho = solve_cholesky_tri_diag<double>(sub_diag, diag, x, j);
+
+         for(size_t i = 1; i < (j-1); i++)
+         {
+            if(abs((sub_diag[i-1]*y_Cho[i-1]+diag[i]*y_Cho[i]+sub_diag[i]*y_Cho[i+1])-x[i]) > epsilon)
+            {
+              verif_Cho = false;
+              erreur_cho = fmax(abs((sub_diag[i-1]*y_Cho[i-1]+diag[i]*y_Cho[i]+sub_diag[i]*y_Cho[i+1])-x[i]),erreur_cho);
+            }
+            if(abs((sub_diag[i-1]*y_LU[i-1]+diag[i]*y_LU[i]+upper_diag[i]*y_LU[i+1])-x[i]) > epsilon)
+            {
+              verif_LU = false;
+              erreur_LU = fmax(abs((sub_diag[i-1]*y_LU[i-1]+diag[i]*y_LU[i]+upper_diag[i]*y_LU[i+1])-x[i]), erreur_LU);
+            }
+         }
+         if(abs((diag[0]*y_Cho[0]+sub_diag[0]*y_Cho[1])-x[0]) > epsilon)
+         {
+            verif_Cho = false;
+         }
+         if(abs((sub_diag[j-2]*y_Cho[j-2]+diag[j-1]*y_Cho[j-1])-x[j-1]) > epsilon)
+         {
+           verif_Cho = false;
+         }
+         if(abs((diag[0]*y_LU[0]+upper_diag[0]*y_LU[1])-x[0]) > epsilon)
+         {
+            verif_LU = false;
+            erreur_LU = fmax(abs((diag[0]*y_LU[0]+upper_diag[0]*y_LU[1])-x[0]), erreur_LU);
+         }
+         if(abs((sub_diag[j-2]*y_LU[j-2]+diag[j-1]*y_LU[j-1])-x[j-1]) > epsilon)
+         {
+           verif_LU = false;
+           erreur_LU = fmax(abs((sub_diag[j-2]*y_LU[j-2]+diag[j-1]*y_LU[j-1])-x[j-1]), erreur_LU);
+         }
+
+         free(y_Cho);
+         free(y_LU);
+         free(x);
+         free(diag);
+         free(sub_diag);
+         free(upper_diag);
+      }
+   }
+
+   cout << "Test 2 LU : Aleatoire" << endl;
+   if(verif_LU)
+   {
+      cout <<"Valide"<<endl;
+   }
+   else
+   {
+      cout << "Echec"<<endl;
+      cout << erreur_LU << endl;
+   }
+
+   cout << "Test 2 Cholesky : Aleatoire" << endl;
+   if(verif_Cho)
+   {
+      cout <<"Valide"<<endl;
+   }
+   else
+   {
+      cout << "Echec"<<endl;
+      cout << erreur_cho << endl;
+   }
+}
+
+void test3_complex_random()
+{
+   bool verif_Cho = true;
+   bool verif_LU = true;
+   double erreur_LU = 0, erreur_cho = 0;
+
+   for(size_t j = MIN_SIZE; j <= MAX_SIZE; j++)
+   {
+      for(size_t k = 0; k < NUMBER_TEST; k++)
+      {
+         double epsilon = PRECISION;
+         complex<double> *diag = (complex<double>*) malloc(j*sizeof(complex<double>)), *sub_diag = (complex<double>*) malloc((j-1)*sizeof(complex<double>)), *upper_diag = (complex<double>*) malloc((j-1)*sizeof(complex<double>)), *x = (complex<double>*) malloc(j*sizeof(complex<double>)), *y_LU, *y_Cho;
+
+         for(size_t i = 0; i < j; i++)
+         {
+            x[i] = (double) alea() + 1.0i * (double) alea();
+            diag[i] = (double) alea() + 1.0i*(double) alea();
+            if(i < (j-1))
+            {
+              sub_diag[i] = (double) alea() + 1.0i * (double) alea();
+              upper_diag[i] = (double) alea() + 1.0i * (double) alea();
+            }
+         }
+
+         y_LU = solve_LU_tri_diag<complex<double>>(sub_diag, diag, upper_diag, x, j);
+         y_Cho = solve_cholesky_tri_diag<complex<double>>(sub_diag, diag, x, j);
+      
+
+         for(size_t i = 1; i < (j-1); i++)
+         {
+            if(abs((sub_diag[i-1]*y_Cho[i-1]+diag[i]*y_Cho[i]+sub_diag[i]*y_Cho[i+1])-x[i]) > epsilon)
+            {
+              verif_Cho = false;
+              erreur_cho = fmax(abs((sub_diag[i-1]*y_Cho[i-1]+diag[i]*y_Cho[i]+sub_diag[i]*y_Cho[i+1])-x[i]),erreur_cho);
+            }
+            if(abs((sub_diag[i-1]*y_LU[i-1]+diag[i]*y_LU[i]+upper_diag[i]*y_LU[i+1])-x[i]) > epsilon)
+            {
+              verif_LU = false;
+              erreur_LU = fmax(abs((sub_diag[i-1]*y_LU[i-1]+diag[i]*y_LU[i]+upper_diag[i]*y_LU[i+1])-x[i]), erreur_LU);
+            }
+         }
+         if(abs((diag[0]*y_Cho[0]+sub_diag[0]*y_Cho[1])-x[0]) > epsilon)
+         {
+            verif_Cho = false;
+         }
+         if(abs((sub_diag[j-2]*y_Cho[j-2]+diag[j-1]*y_Cho[j-1])-x[j-1]) > epsilon)
+         {
+           verif_Cho = false;
+         }
+         if(abs((diag[0]*y_LU[0]+upper_diag[0]*y_LU[1])-x[0]) > epsilon)
+         {
+            verif_LU = false;
+            erreur_LU = fmax(abs((diag[0]*y_LU[0]+upper_diag[0]*y_LU[1])-x[0]), erreur_LU);
+         }
+         if(abs((sub_diag[j-2]*y_LU[j-2]+diag[j-1]*y_LU[j-1])-x[j-1]) > epsilon)
+         {
+           verif_LU = false;
+           erreur_LU = fmax(abs((sub_diag[j-2]*y_LU[j-2]+diag[j-1]*y_LU[j-1])-x[j-1]), erreur_LU);
+         }
+
+         free(y_Cho);
+         free(y_LU);
+         free(x);
+         free(diag);
+         free(sub_diag);
+         free(upper_diag);
+      }
+   }
+
+   cout << "Test 3 LU : Aleatoire Complexe" << endl;
+   if(verif_LU)
+   {
+      cout <<"Valide"<<endl;
+   }
+   else
+   {
+      cout << "Echec"<<endl;
+      cout << erreur_LU << endl;
+   }
+
+   cout << "Test 3 Cholesky : Aleatoire Complexe" << endl;
+   if(verif_Cho)
+   {
+      cout <<"Valide"<<endl;
+   }
+   else
+   {
+      cout << "Echec"<<endl;
+      cout << erreur_cho << endl;
+   }
 }
